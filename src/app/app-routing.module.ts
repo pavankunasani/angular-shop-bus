e3d0a6828261b5f4a9066dc8713c56a4bcda88e5
pavankunasani <pavankunasani@users.noreply.github.com>
2022-12-19 14:05:32 +0530

    1 file modified, 2 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer/customer.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [{ path: 'customers', component: CustomerComponent }];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'customers', component: CustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), CommonModule],
